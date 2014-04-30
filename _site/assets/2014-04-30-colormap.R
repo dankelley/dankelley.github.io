e610388071b1a0d7f9b3bc438eaf25bffbfaa452
Dan Kelley <kelley.dan@gmail.com>
2014-04-30 08:57:00 -0300

    colormap

---
+++ _site/assets/2014-04-30-colormap.R
@@ -0,0 +1,51 @@
+
+## ----colormap, fig.height=5, fig.path='2014-04-30-', dpi=100-------------
+library(oce)
+## Example 1. color scheme for points on xy plot
+x <- seq(0, 1, length.out=40)
+y <- sin(2 * pi * x)
+par(mar=c(3, 3, 1, 1))
+mar <- par('mar') # prevent margin creep by drawPalette()
+## First, default breaks
+c <- Colormap(y)
+drawPalette(c$zlim, col=c$col, breaks=c$breaks)
+plot(x, y, bg=c$zcol, pch=21, cex=1)
+grid()
+par(mar=mar)
+## Second, 100 breaks, yielding a smoother palette
+c <- Colormap(y, breaks=100)
+drawPalette(c$zlim, col=c$col, breaks=c$breaks)
+plot(x, y, bg=c$zcol, pch=21, cex=1)
+grid()
+par(mar=mar)
+
+## Example 2. topographic image with a standard color scheme
+par(mfrow=c(1,1))
+data(topoWorld)
+cm <- Colormap(name="gmt_globe")
+imagep(topoWorld, breaks=cm$breaks, col=cm$col)
+
+## Example 3. topographic image with modified colors
+cm <- Colormap(name="gmt_globe")
+deep <- cm$x0 <= -4000
+cm$col0[deep] <- 'black'
+cm$col1[deep] <- 'black'
+cm <- Colormap(x0=cm$x0, x1=cm$x1, col0=cm$col0, col1=cm$col1)
+imagep(topoWorld, breaks=cm$breaks, col=cm$col)
+
+## Example 4. image of world topography with water colorized 
+##            smoothly from violet at 8km depth to blue
+##            at 4km depth, then blending in 0.5km increments
+##            to white at the coast, with tan for land.
+cm <- Colormap(x0=c(-8000, -4000, 0, 100),
+               x1=c(-8000, -4000, 0, 100),
+               col0=c("violet","blue","white","tan"),
+               col1=c("violet","blue","white","tan"),
+               n=c(100, 8, 1))
+lon <- topoWorld[['longitude']]
+lat <- topoWorld[['latitude']]
+z <- topoWorld[['z']]
+imagep(lon, lat, z, breaks=cm$breaks, col=cm$col)
+contour(lon, lat, z, levels=0, add=TRUE)
+
+
