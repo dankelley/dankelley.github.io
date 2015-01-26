ddfcf6cbf8eaa9830a1c18c881a1c55c52b1fde8
Dan Kelley <kelley.dan@gmail.com>
2015-01-26 15:05:45 -0400

    levitus

--- _site/assets/2013-12-21-solstice.R
+++ _site/assets/2013-12-21-solstice.R
@@ -1,16 +1,15 @@
 
 ## ------------------------------------------------------------------------
-t <- as.POSIXct("2013-12-21 17:11:00", tz="UTC") # winter solstice
+t0 <- as.POSIXct("2014-12-21 10:00:00", tz="UTC") # morning of 
 xy <- list(x=-63.60, y=44.65)          # centre of map (Halifax)
-D <- 8                                 # map span in km
+D <- 9                                 # map span in km
 
 
 ## ----,message=FALSE,warning=FALSE----------------------------------------
 library(oce)
-t0 <- t - 86400 / 4
 sunrise <- uniroot(function(t)
-                   sunAngle(t, lat=xy$y, lon=xy$x)$altitude,
-                   interval=as.numeric(c(t0, t)))$root
+                   sunAngle(t, lat=xy$y, lon=xy$x, useRefraction=TRUE)$altitude,
+                   interval=as.numeric(t0 + 3600*c(-5,5)))$root
 sunrise <- numberAsPOSIXct(sunrise)
 azimuth <- 90 - sunAngle(sunrise, lat=xy$y, lon=xy$x)$azimuth
 
@@ -20,10 +19,8 @@ D <- D / 111                           # deg
 Dlon <- D / cos(xy$y * pi / 180)
 
 
-## ----message=FALSE, warning=FALSE----------------------------------------
+## ----solstice-map,fig.height=5,fig.width=5,dpi=150,message=FALSE, warning=FALSE----
 library(OpenStreetMap)
-
-## ----solstice-map,fig.height=7,fig.width=7,dpi=120-----------------------
 map <- openmap(c(lat=xy$y+D/2, lon=xy$x-Dlon/2),
                c(lat=xy$y-D/2, lon=xy$x+Dlon/2),
                minNumTiles=9)
@@ -32,9 +29,10 @@ plot(map)
 cx <- mean(par('usr')[1:2])
 cy <- mean(par('usr')[3:4])
 d <- diff(par('usr')[3:4]) # scales as the map
-for (o in d*seq(-1, 1, length.out=30)) {
+for (o in d*seq(-1, 1, length.out=60)) {
     lines(cx+c(-1,1)*d*cos(azimuth*pi/180),
           cy+o+c(-1,1)*d*sin(azimuth*pi/180), col='red')
 }
+mtext(paste("Solstice sunrise at ", format(sunrise-4*3600, "%Y-%m-%d %H:%M")), font=2)
 
 
