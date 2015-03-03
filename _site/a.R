9532af1840e2b7480606015d960590fe9daadf06
Dan Kelley <kelley.dan@gmail.com>
2015-03-03 08:52:13 -0400

    trajectory

---
+++ _site/a.R
@@ -0,0 +1,20 @@
+library(oce)
+x <- seq(0, 1, length.out=50)
+y <- x
+z <- seq(0, 1, length.out = length(x))
+zlim <- range(z)
+npalette <- 10
+mar <- par('mar')
+
+palette <- oceColorsJet(npalette)
+drawPalette(zlim = zlim, col = palette)
+par(mar=mar)
+plot(x, y, type = "l")
+grid()
+segments(head(x, -1), head(y, -1),
+  tail(x, -1), tail(y, -1),
+  col = palette[findInterval(z, 
+    seq(zlim[1], zlim[2], length.out = npalette + 1))],
+  lwd = 8)
+points(x, y, pch = 20, cex=1/3)
+
