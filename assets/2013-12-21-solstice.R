bc2e05a76de408f05146e2e78011bafb988fda06
dankelley <kelley.dan@gmail.com>
2020-11-18 13:12:40 -0400

    .

--- assets/2013-12-21-solstice.R
+++ assets/2013-12-21-solstice.R
@@ -1,11 +1,10 @@
-
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 t0 <- as.POSIXct("2014-12-21 10:00:00", tz="UTC") # morning of 
 xy <- list(x=-63.60, y=44.65)          # centre of map (Halifax)
 D <- 9                                 # map span in km
 
 
-## ----,message=FALSE,warning=FALSE----------------------------------------
+## ----message=FALSE,warning=FALSE----------------------------------------------
 library(oce)
 sunrise <- uniroot(function(t)
                    sunAngle(t, lat=xy$y, lon=xy$x, useRefraction=TRUE)$altitude,
@@ -14,7 +13,7 @@ sunrise <- numberAsPOSIXct(sunrise)
 azimuth <- 90 - sunAngle(sunrise, lat=xy$y, lon=xy$x)$azimuth
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 D <- D / 111                           # deg
 Dlon <- D / cos(xy$y * pi / 180)
 
@@ -34,5 +33,4 @@ for (o in d*seq(-1, 1, length.out=60)) {
           cy+o+c(-1,1)*d*sin(azimuth*pi/180), col='red')
 }
 mtext(paste("Solstice sunrise at ", format(sunrise-4*3600, "%Y-%m-%d %H:%M")), font=2)
-
 
