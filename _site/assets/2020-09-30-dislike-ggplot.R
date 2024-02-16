87b07268ae5cf5e2bbd4ed81b318121da19b5ca2
dankelley <kelley.dan@gmail.com>
2024-02-16 19:13:46 -0400

    remove old blog items (doing wrong before)

--- _site/assets/2020-09-30-dislike-ggplot.R
+++
@@ -1,9 +0,0 @@
-## ----eval=FALSE---------------------------------------------------------------
-## d <- data.frame(x=rnorm(1e8))
-## system.time(hist(x))
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## library(ggplot2)
-## system.time({p<-ggplot(d) + aes(x=x) + geom_histogram();print(p)})
-
